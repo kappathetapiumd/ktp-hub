ed0a0fbb594a64975a7910c9f743363fb245d7c2
nikhil-shyam <nikhildshyam@gmail.com>
2026-06-18 22:18:03 -0400

    selected the current week based off today

--- src/components/strikes/Weeks.tsx
+++ src/components/strikes/Weeks.tsx
@@ -1,13 +1,14 @@
 import { useRef, useState } from 'react';
+import dayjs from 'dayjs';
 import './Weeks.modules.css';
 
 const weeks = [
   "3/2 - 3/8", "3/9 - 3/15", "3/16 - 3/22", "3/23 - 3/29", "3/30 - 4/5",
-  "4/6 - 4/12", "4/13 - 4/19", "4/20 - 4/26", "4/27 - 5/3", "5/4 - 5/8"
+  "4/6 - 4/12", "4/13 - 4/19", "4/20 - 4", "4/27 - 5/3", "5/4 - 5/8"
 ];
 
 export default function Weeks() {
-  const [activeWeek, setActiveWeek] = useState(weeks[0]);
+  const [activeWeek, setActiveWeek] = useState(getCurrentWeek(dayjs()));
   
   const scrollRef = useRef<HTMLDivElement>(null);
   const isMouseDown = useRef(false);
@@ -23,7 +24,6 @@ export default function Weeks() {
     hasDragged.current = false;
     startX.current = e.pageX;
     scrollLeft.current = element.scrollLeft;
-    element.classList.add('dragging');
   }
 
   function handleMouseMove(e: React.MouseEvent<HTMLDivElement>) {
@@ -32,8 +32,10 @@ export default function Weeks() {
 
     const distance = e.pageX - startX.current;
 
-    if (Math.abs(distance) > 5)
+    if (Math.abs(distance) > 5 && !hasDragged.current) {
       hasDragged.current = true;
+      element.classList.add('dragging');
+    }
 
     if (hasDragged.current) {
       e.preventDefault();
@@ -42,7 +44,8 @@ export default function Weeks() {
   }
 
   function stopDragging() {
-    isMouseDown.current = false
+    isMouseDown.current = false;
+    hasDragged.current = false;
     scrollRef.current?.classList.remove('dragging');
   }
 
@@ -56,12 +59,15 @@ export default function Weeks() {
       onMouseLeave={stopDragging}
     >
       <div className="weeks-container">
-        {weeks.map((week, index) => {
+        {weeks.map((week, index) => {          
           return (
             <button
-              onClick={() => hasDragged.current || setActiveWeek(week)}
+              onClick={() => {
+                if (hasDragged.current) return;
+                setActiveWeek(week);
+              }}
               key={week + index}
-              className={`week-btn ${activeWeek === week && 'active'}`}
+              className={`week-btn ${activeWeek === week ? 'active' : ''}`}
             >
               <span className="week-label">Week {index + 1}</span>
               <span className="week-dates">{week}</span>
@@ -71,4 +77,15 @@ export default function Weeks() {
       </div>
     </div>
   );
+}
+function getCurrentWeek(today: dayjs.Dayjs) {
+  for (let i = weeks.length - 1; i >= 0; i--) {
+    const [start] = weeks[i].split(' - ');
+    const startOfWeek = dayjs(start);
+
+    if (today.isAfter(startOfWeek) || today.isSame(startOfWeek, 'day'))
+      return weeks[i];
+  }
+
+  return weeks[0];
 }
