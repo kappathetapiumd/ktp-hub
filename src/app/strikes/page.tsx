c063b33b34c25d349abd690e82575f924f4b38ab
nikhil-shyam <nikhildshyam@gmail.com>
2026-06-19 05:22:39 -0400

    added network background (unnecessary) to strike dashboard

--- src/app/strikes/page.tsx
+++ src/app/strikes/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useMediaQuery } from 'react-responsive';
+
+import NetworkBackground from '@/components/login/NetworkBackground';
 import PledgeSideBar from '@/components/strikes/PledgeSideBar';
 import StrikeInput from '@/components/strikes/StrikeInput';
 import WeeksSelect from "@/components/strikes/WeeksSelect";
@@ -7,22 +10,30 @@ import StrikeHistory from '@/components/strikes/StrikeHistory';
 import styles from './page.module.css';
 
 export default function Strikes() {
+  const isMobile = useMediaQuery({ maxWidth: 520 });
+
   return (
-    <main className={styles['dashboard']}>
-      <aside className={styles['sidebar']}>
-        <PledgeSideBar />
-      </aside>
+    <>
+      {!isMobile && <NetworkBackground /> /* might dtm - possibly get rid of this, also kinda buggy on ipad */ }
+      <main className={styles['dashboard']}>
 
-      <section className={styles['dashboard-main']}>
-        <div className={styles['dashboard-controls']}>
-          <StrikeInput />
-          <WeeksSelect />
-        </div>
+        <aside className={styles['sidebar']}>
+          <PledgeSideBar />
+        </aside>
 
-        <div className={styles['history-wrapper']}>
-          <StrikeHistory />
-        </div>
-      </section>
-    </main>
+        <section className={styles['dashboard-main']}>
+          {/* <NetworkBackground /> */}
+
+          <div className={styles['dashboard-controls']}>
+            <StrikeInput />
+            <WeeksSelect />
+          </div>
+
+          <div className={styles['history-wrapper']}>
+            <StrikeHistory />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
